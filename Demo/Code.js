bd970fef0633e9c1ae2e8a82c67fe9c22eeab496
Charles McCloud <coolmantackle13@gmail.com>
2019-02-26 19:04:35 -0500

    W.I.P

--- Demo/Code.js
+++ Demo/Code.js
@@ -1,11 +1,29 @@
-let main_health = 100
-let boss_health = 200
-let damage = 0
+let main_health = 100;
+let boss_health = 150;
+let p_damage = 0;
+let b_damage = 0;
+let special = 0;
 
-function attack() {
-  damage = Math.floor((Math.random() * 23) + 7);
+function p_attack() {
+  p_damage = Math.floor((Math.random() * 23) + 7);
+  boss_health = boss_health - p_damage;
+  special = special + 1;
 }
+function b_attack() {
+  b_damage = Math.floor((Math.random() * 6) + 10);
+  main_health = main_health - b_damage
+}
+  function sattack() {
+    if (special >= 4) {
+      boss_health = boss_health - 30;
+      special = 0
+    } else {
+      alert("You do not have enough special energy.")
+    }
+  }
 
 function trial()  {
-  console.log(damage)
+  console.log(p_damage);
+  console.log(boss_health);
+  console.log(special);
 }
