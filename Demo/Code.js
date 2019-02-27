7e4edb45a59f16ccffe3e3e2bd775099787a36e6
Charles McCloud <coolmantackle13@gmail.com>
2019-02-27 10:48:34 -0500

    W.I.P

--- Demo/Code.js
+++ Demo/Code.js
@@ -3,27 +3,45 @@ let boss_health = 150;
 let p_damage = 0;
 let b_damage = 0;
 let special = 0;
+let potions = 4;
 
+// (Boss functions)
+function b_attack() {
+  b_damage = Math.floor((Math.random() * 6) + 10);
+  main_health = main_health - b_damage;
+  console.log("The Boss did " + b_damage + " damage.");
+  display();
+}
+
+// (Player Functions)
 function p_attack() {
   p_damage = Math.floor((Math.random() * 23) + 7);
   boss_health = boss_health - p_damage;
   special = special + 1;
+  console.log("You did " + p_damage + " damage.");
+  b_attack();
 }
-function b_attack() {
-  b_damage = Math.floor((Math.random() * 6) + 10);
-  main_health = main_health - b_damage
-}
-  function sattack() {
+function sattack() {
     if (special >= 4) {
       boss_health = boss_health - 30;
-      special = 0
+      special = 0;
+      b_attack();
     } else {
-      alert("You do not have enough special energy.")
+      console.log("You do not have enough special energy.");
     }
   }
-
-function trial()  {
-  console.log(p_damage);
-  console.log(boss_health);
-  console.log(special);
+function potion() {
+  if (potions > 0) {
+    main_health = main_health + 30;
+    potions = potions - 1;
+    b_attack();
+  } else {
+    console.log("You do not have enough potions.");
+  }
+}
+function display()  {
+  console.log("Health:",main_health);
+  console.log("Potions:",potions)
+  console.log("Special Meter:",special);
+  console.log("Boss Health:",boss_health);
 }
